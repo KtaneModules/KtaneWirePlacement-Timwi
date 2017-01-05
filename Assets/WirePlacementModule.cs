339e8a8b65002a09af136c760c9d425cd37d7c86
Timwi <github@timwi.de>
2017-01-05 16:11:00 +0100

    Blank project.

---
+++ Assets/WirePlacementModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WirePlacement;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Wire Placement
+/// Created by Timwi
+/// </summary>
+public class WirePlacementModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    void Start()
+    {
+        Debug.Log("[Wire Placement] Started");
+    }
+
+    void ActivateModule()
+    {
+        Debug.Log("[Wire Placement] Activated");
+    }
+}
