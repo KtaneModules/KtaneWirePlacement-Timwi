4912acf60f677748b08e31e1f703dc5cdc7ea84b
Timwi <github@timwi.de>
2017-01-06 18:27:19 +0100

    Wire mesh generator (mostly taken from Follow The Leader). Add Hexi’s TestHarness.

---
+++ Assets/Ut.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rnd = UnityEngine.Random;
+
+namespace WirePlacement
+{
+    static class Ut
+    {
+        /// <summary>
+        ///     Brings the elements of the given list into a random order.</summary>
+        /// <typeparam name="T">
+        ///     Type of elements in the list.</typeparam>
+        /// <param name="list">
+        ///     List to shuffle.</param>
+        /// <returns>
+        ///     The list operated on.</returns>
+        public static IList<T> Shuffle<T>(this IList<T> list)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            for (int j = list.Count; j >= 1; j--)
+            {
+                int item = Rnd.Range(0, j);
+                if (item < j - 1)
+                {
+                    var t = list[item];
+                    list[item] = list[j - 1];
+                    list[j - 1] = t;
+                }
+            }
+            return list;
+        }
+
+        public static T[] NewArray<T>(params T[] array) { return array; }
+
+        /// <summary>
+        ///     Turns all elements in the enumerable to strings and joins them using the specified <paramref
+        ///     name="separator"/> and the specified <paramref name="prefix"/> and <paramref name="suffix"/> for each string.</summary>
+        /// <param name="values">
+        ///     The sequence of elements to join into a string.</param>
+        /// <param name="separator">
+        ///     Optionally, a separator to insert between each element and the next.</param>
+        /// <param name="prefix">
+        ///     Optionally, a string to insert in front of each element.</param>
+        /// <param name="suffix">
+        ///     Optionally, a string to insert after each element.</param>
+        /// <param name="lastSeparator">
+        ///     Optionally, a separator to use between the second-to-last and the last element.</param>
+        /// <example>
+        ///     <code>
+        ///         // Returns "[Paris], [London], [Tokyo]"
+        ///         (new[] { "Paris", "London", "Tokyo" }).JoinString(", ", "[", "]")
+        ///         
+        ///         // Returns "[Paris], [London] and [Tokyo]"
+        ///         (new[] { "Paris", "London", "Tokyo" }).JoinString(", ", "[", "]", " and ");</code></example>
+        public static string JoinString<T>(this IEnumerable<T> values, string separator = null, string prefix = null, string suffix = null, string lastSeparator = null)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+            if (lastSeparator == null)
+                lastSeparator = separator;
+
+            using (var enumerator = values.GetEnumerator())
+            {
+                if (!enumerator.MoveNext())
+                    return "";
+
+                // Optimise the case where there is only one element
+                var one = enumerator.Current;
+                if (!enumerator.MoveNext())
+                    return prefix + one + suffix;
+
+                // Optimise the case where there are only two elements
+                var two = enumerator.Current;
+                if (!enumerator.MoveNext())
+                {
+                    // Optimise the (common) case where there is no prefix/suffix; this prevents an array allocation when calling string.Concat()
+                    if (prefix == null && suffix == null)
+                        return one + lastSeparator + two;
+                    return prefix + one + suffix + lastSeparator + prefix + two + suffix;
+                }
+
+                StringBuilder sb = new StringBuilder()
+                    .Append(prefix).Append(one).Append(suffix).Append(separator)
+                    .Append(prefix).Append(two).Append(suffix);
+                var prev = enumerator.Current;
+                while (enumerator.MoveNext())
+                {
+                    sb.Append(separator).Append(prefix).Append(prev).Append(suffix);
+                    prev = enumerator.Current;
+                }
+                sb.Append(lastSeparator).Append(prefix).Append(prev).Append(suffix);
+                return sb.ToString();
+            }
+        }
+
+        public static T PickRandom<T>(this IEnumerable<T> src)
+        {
+            if (src == null)
+                throw new ArgumentNullException("src");
+
+            var arr = src.ToArray();
+            if (arr.Length == 0)
+                throw new InvalidOperationException("Cannot pick a random element from an empty set.");
+            return arr[Rnd.Range(0, arr.Length)];
+        }
+
+        /// <summary>
+        ///     Enumerates all consecutive pairs of the elements.</summary>
+        /// <param name="source">
+        ///     The input enumerable.</param>
+        /// <param name="closed">
+        ///     If true, an additional pair containing the last and first element is included. For example, if the source
+        ///     collection contains { 1, 2, 3, 4 } then the enumeration contains { (1, 2), (2, 3), (3, 4) } if <paramref
+        ///     name="closed"/> is <c>false</c>, and { (1, 2), (2, 3), (3, 4), (4, 1) } if <paramref name="closed"/> is
+        ///     <c>true</c>.</param>
+        /// <param name="selector">
+        ///     The selector function to run each consecutive pair through.</param>
+        public static IEnumerable<TResult> SelectConsecutivePairs<T, TResult>(this IEnumerable<T> source, bool closed, Func<T, T, TResult> selector)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (selector == null)
+                throw new ArgumentNullException("selector");
+            return selectConsecutivePairsIterator(source, closed, selector);
+        }
+        private static IEnumerable<TResult> selectConsecutivePairsIterator<T, TResult>(IEnumerable<T> source, bool closed, Func<T, T, TResult> selector)
+        {
+            using (var enumer = source.GetEnumerator())
+            {
+                bool any = enumer.MoveNext();
+                if (!any)
+                    yield break;
+                T first = enumer.Current;
+                T last = enumer.Current;
+                while (enumer.MoveNext())
+                {
+                    yield return selector(last, enumer.Current);
+                    last = enumer.Current;
+                }
+                if (closed)
+                    yield return selector(last, first);
+            }
+        }
+
+        public static IEnumerable<TResult> SelectManyConsecutivePairs<T, TResult>(this IEnumerable<T> source, bool closed, Func<T, T, IEnumerable<TResult>> selector)
+        {
+            return source.SelectConsecutivePairs(closed, selector).SelectMany(x => x);
+        }
+
+        /// <summary>
+        ///     Enumerates the items of this collection, skipping the last <paramref name="count"/> items. Note that the
+        ///     memory usage of this method is proportional to <paramref name="count"/>, but the source collection is only
+        ///     enumerated once, and in a lazy fashion. Also, enumerating the first item will take longer than enumerating
+        ///     subsequent items.</summary>
+        /// <param name="source">
+        ///     Source collection.</param>
+        /// <param name="count">
+        ///     Number of items to skip from the end of the collection.</param>
+        /// <param name="throwIfNotEnough">
+        ///     If <c>true</c>, the enumerator throws at the end of the enumeration if the source collection contained fewer
+        ///     than <paramref name="count"/> elements.</param>
+        public static IEnumerable<T> SkipLast<T>(this IEnumerable<T> source, int count, bool throwIfNotEnough = false)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count", "count cannot be negative.");
+            if (count == 0)
+                return source;
+
+            var collection = source as ICollection<T>;
+            if (collection != null)
+            {
+                if (throwIfNotEnough && collection.Count < count)
+                    throw new InvalidOperationException("The collection does not contain enough elements.");
+                return collection.Take(Math.Max(0, collection.Count - count));
+            }
+
+            return skipLastIterator(source, count, throwIfNotEnough);
+        }
+        private static IEnumerable<T> skipLastIterator<T>(IEnumerable<T> source, int count, bool throwIfNotEnough)
+        {
+            var queue = new T[count];
+            int headtail = 0; // tail while we're still collecting, both head & tail afterwards because the queue becomes completely full
+            int collected = 0;
+
+            foreach (var item in source)
+            {
+                if (collected < count)
+                {
+                    queue[headtail] = item;
+                    headtail++;
+                    collected++;
+                }
+                else
+                {
+                    if (headtail == count)
+                        headtail = 0;
+                    yield return queue[headtail];
+                    queue[headtail] = item;
+                    headtail++;
+                }
+            }
+
+            if (throwIfNotEnough && collected < count)
+                throw new InvalidOperationException("The collection does not contain enough elements.");
+        }
+
+        /// <summary>
+        ///     Returns a collection containing only the last <paramref name="count"/> items of the input collection. This
+        ///     method enumerates the entire collection to the end once before returning. Note also that the memory usage of
+        ///     this method is proportional to <paramref name="count"/>.</summary>
+        public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> source, int count)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count", "count cannot be negative.");
+            if (count == 0)
+                return new T[0];
+
+            var collection = source as ICollection<T>;
+            if (collection != null)
+                return collection.Skip(Math.Max(0, collection.Count - count));
+
+            var queue = new Queue<T>(count + 1);
+            foreach (var item in source)
+            {
+                if (queue.Count == count)
+                    queue.Dequeue();
+                queue.Enqueue(item);
+            }
+            return queue.AsEnumerable();
+        }
+    }
+}
--- Assets/WirePlacementModule.cs
+++ Assets/WirePlacementModule.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using WirePlacement;
 using UnityEngine;
+
 using Rnd = UnityEngine.Random;
 
 /// <summary>
 /// On the Subject of Wire Placement
-/// Created by Timwi
+/// Created by lumbud84, implemented by Timwi
 /// </summary>
 public class WirePlacementModule : MonoBehaviour
 {
@@ -15,9 +16,67 @@ public class WirePlacementModule : MonoBehaviour
     public KMBombModule Module;
     public KMAudio Audio;
 
+    public KMSelectable MainSelectable;
+    public Material[] WireMaterials;
+    public Material CopperMaterial;
+
     void Start()
     {
         Debug.Log("[Wire Placement] Started");
+
+        for (int i = 0; i < 8; i++)
+        {
+            var j = i;
+            var seg = Rnd.Range(3, 5);
+            var seed = Rnd.Range(0, int.MaxValue);
+            var wireColor = Rnd.Range(0, WireMaterials.Length);
+
+            var wireObj = MainSelectable.transform.Find(string.Format("Wire {0}", i + 1));
+            wireObj.GetComponent<MeshFilter>().mesh = MeshGenerator.GenerateWire(.0304, seg, MeshGenerator.WirePiece.Uncut, false, seed);
+            wireObj.GetComponent<MeshRenderer>().material = WireMaterials[wireColor];
+
+            wireObj.localPosition = new Vector3(-0.0608f * (i % 2), .029f, 0.0304f * (i / 2) - 0.0608f);
+            wireObj.localScale = new Vector3(1, 1, 1);
+            wireObj.localEulerAngles = new Vector3(0, 0, 0);
+
+            var wireHighlight = wireObj.transform.Find(string.Format("Wire {0} highlight", i + 1));
+            var highlightMesh = MeshGenerator.GenerateWire(.0304, seg, MeshGenerator.WirePiece.Uncut, true, seed);
+            wireHighlight.GetComponent<MeshFilter>().mesh = highlightMesh;
+            wireHighlight.localPosition = new Vector3(0, 0, 0);
+            wireHighlight.localScale = new Vector3(1, 1, 1);
+            wireHighlight.localEulerAngles = new Vector3(0, 0, 0);
+
+            var wireHighlightClone = wireHighlight.Find("Highlight(Clone)");
+            if (wireHighlightClone != null)
+                wireHighlightClone.GetComponent<MeshFilter>().mesh = highlightMesh;
+
+            var cutMesh = MeshGenerator.GenerateWire(.0304, seg, MeshGenerator.WirePiece.Cut, false, seed);
+            var cutHighlightMesh = MeshGenerator.GenerateWire(.0304, seg, MeshGenerator.WirePiece.Cut, true, seed);
+            var copperMesh = MeshGenerator.GenerateWire(.0304, seg, MeshGenerator.WirePiece.Copper, false, seed);
+            var interacted = false;
+
+            wireObj.GetComponent<KMSelectable>().OnInteract = delegate
+            {
+                if (interacted)
+                    return false;
+                interacted = true;
+
+                wireObj.GetComponent<MeshFilter>().mesh = cutMesh;
+                var copperObj = new GameObject { name = string.Format("Wire {0} copper", j + 1) }.transform;
+                copperObj.parent = wireObj;
+                copperObj.localPosition = new Vector3(0, 0, 0);
+                copperObj.localEulerAngles = new Vector3(0, 0, 0);
+                copperObj.localScale = new Vector3(1, 1, 1);
+                copperObj.gameObject.AddComponent<MeshFilter>().mesh = copperMesh;
+                copperObj.gameObject.AddComponent<MeshRenderer>().material = CopperMaterial;
+
+                wireHighlight.GetComponent<MeshFilter>().mesh = cutHighlightMesh;
+                wireHighlightClone = wireHighlight.Find("Highlight(Clone)");
+                if (wireHighlightClone != null)
+                    wireHighlightClone.GetComponent<MeshFilter>().mesh = cutHighlightMesh;
+                return false;
+            };
+        }
     }
 
     void ActivateModule()
